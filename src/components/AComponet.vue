7e4ae920e3535b9290b8da65dce499d95b1ae9de
钟林钦 <rainkyzhong@sina.com>
2024-07-29 14:48:52 +0800

    feat

---
+++ src/components/AComponet.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>我是一个异步组件</div>
+</template>
+<script lang="ts">
+console.log('我被执行了');
+export default {
+    created() {
+        console.log('我被创建了');
+    },
+    mounted() {
+        console.log('我被挂载了');
+    }
+}
+</script>
---
+++ src/views/AsyncComponentTest.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <!-- 通过v-if控制,当需要显示的时候才去加载 -->
+    <button @click="showComponent = true">显示组件</button>
+    <AComponent v-if="showComponent" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineAsyncComponent, ref, } from 'vue';
+const showComponent = ref<boolean>(false)
+
+/**
+ * 同步加载组件
+ */
+// import AComponent from '@/components/AComponet.vue'
+
+/**
+ * 异步加载组件
+ */
+const AComponent = defineAsyncComponent(() => {
+  return import('@/components/AComponet.vue');
+})
+</script>
